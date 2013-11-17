8eb2ef1251dce2fdb82dfe0322ea089e2f25f987
savas <savasaydin@gmail.com>
2013-11-17 10:37:46 +0100

    Complete valid and invalid integer decoding

--- src/torrent_decoder.erl
+++ src/torrent_decoder.erl
@@ -3,24 +3,29 @@
 -export([decode/1
 	]).
 
-decode(<<$i,_ByteInteger/binary>> = Bin) ->
-    InStringFormat = decode_integer(Bin),
-    list_to_integer(InStringFormat).
+-record(metadata, {string_byte,
+		  integer,
+		  list,
+		  dictionary}).
 
-decode_integer(<<$i,$0,Integer,_Rest/binary>>) when Integer >= $0 andalso
-						   Integer =< $9 ->
-    throw({error, invalid_integer});
-decode_integer(<<$i,$-,Integer,Rest/binary>>) when Integer > $0 andalso
-						  Integer =< $9 ->
-    [$-, Integer | decode_integer(Rest)];
-decode_integer(<<$i,Rest/binary>>) ->
-    decode_integer(Rest);
-decode_integer(<<$e,_/binary>>) ->
-    [];
-decode_integer(<<Integer,Rest/binary>>) when Integer >= $0 andalso
+decode(<<$i,ByteInteger/binary>>) ->
+    InStringFormat = decode_integer(ByteInteger, []),
+    #metadata{integer = list_to_integer(InStringFormat)}.
+
+decode_integer(<<$e,_/binary>>,Result) ->
+    lists:reverse(Result);
+decode_integer(<<$-,$0,_/binary>>,Result) when Result == [] ->
+    error(invalid_integer_format);
+decode_integer(<<$-,Rest/binary>>,Result) when Result == [] ->
+    decode_integer(Rest, [$-|Result]);
+decode_integer(<<$0,Integer,_/binary>>,Result) when Result == [] andalso
+						    Integer >= $0 andalso
+						    Integer =< $9 ->
+    error(invalid_integer_format);
+decode_integer(<<Integer,Rest/binary>>, Result) when Integer >= $0 andalso
 					    Integer =< $9 ->
-    [Integer | decode_integer(Rest)];
-decode_integer(_) ->
-    throw({error, invalid_integer}).
-
+    decode_integer(Rest, [Integer|Result]);
+decode_integer(_,_) ->
+    error(invalid_integer_format).
     
+
--- test/torrent_decoder_tests.erl
+++ test/torrent_decoder_tests.erl
@@ -2,6 +2,11 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-record(metadata, {string_byte,
+		  integer,
+		  list,
+		  dictionary}).
+
 decode_integer_test_() ->
     [fun valid_integer_alberts_birthdate/0,
      fun valid_integer_end_with_e_disclosure/0,
@@ -16,42 +21,51 @@ decode_integer_test_() ->
     ].
 
 valid_integer_alberts_birthdate() ->
-    ?assertEqual(20130922, 
-		 torrent_decoder:decode(<<"i20130922e">>)).
+    Metadata = torrent_decoder:decode(<<"i20130922e">>),
+    ?assertEqual(20130922, Metadata#metadata.integer). 
+
 
 valid_integer_end_with_e_disclosure() ->
-    ?assertEqual(20130922, 
-		 torrent_decoder:decode(<<"i20130922e5:">>)).
+    Metadata = torrent_decoder:decode(<<"i20130922e5:">>),
+    ?assertEqual(20130922, Metadata#metadata.integer). 
+
 
 valid_integer_zero() ->
-    ?assertEqual(0, 
-		 torrent_decoder:decode(<<"i0e">>)).    
+    Metadata = torrent_decoder:decode(<<"i0e">>),
+    ?assertEqual(0, Metadata#metadata.integer). 
 
 valid_negative_integer() ->
-    ?assertEqual(-3, 
-		 torrent_decoder:decode(<<"i-3e">>)).    
+    Metadata = torrent_decoder:decode(<<"i-3e">>),
+    ?assertEqual(-3, Metadata#metadata.integer).
+
 
 invalid_integer_format_due_to_no_e_enclosure() ->
-  ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"i12345">>)).
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"i12345">>)).
 
 invalid_integer_format_due_to_not_integer() ->
-  ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"inot_inte">>)).
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"inot_inte">>)).
     
 invalid_integer_due_to_dot() ->
-    ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"i2013.09.22e">>)).
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"i2013.09.22e">>)).
 
 invalid_integer_due_to_minus_after_an_integer() ->
-    ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"i2-3e">>)).    
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"i2-3e">>)).    
 
 invalid_integer_due_to_zero_with_another_integer() ->
-    ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"i02e">>)).    
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"i02e">>)).    
 
 invalid_integer_due_to_minus_before_zero() ->
-    ?assertThrow({error, invalid_integer}, 
-		 torrent_decoder:decode(<<"i-0e">>)).    
+    ?assertException(error, 
+		     invalid_integer_format, 
+		     torrent_decoder:decode(<<"i-0e">>)).    
 
